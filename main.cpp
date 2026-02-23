5916867a6d32267c69736439c0e38c7c4b18ad95
smann9915 <smann9915@sdsu.edu>
2026-02-23 14:09:14 -0800

    Basic Dice System

--- main.cpp
+++ main.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
+#include <random>
+#include <string>
 
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+using namespace std;
+
+int rollDice() {
+    int dice1 =  rand() % 6 + 1;
+    int dice2 = rand() % 6 + 1;
+
+    return dice1 + dice2;
+}
+
+void playTurn() {
+    //Rolls the dice
+    string answer = "";
+    cout << "Enter anything to roll the dice: ";
+    cin >> answer;
+    cout << rollDice() << endl;
+}
+
 int main() {
-    //play monopoly
+    //temporary gameplay loop
+    for (int i = 1; i <= 12; i++) {
+        playTurn();
+    }
 }
